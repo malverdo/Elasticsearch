222e86c360bba025b1d50c8558adc91d1bc6b354
malverdo <malverdo11@mail.ru>
2022-01-24 09:05:00 +0300

    Then CreateClientElasticSearch

--- Elasticsearch/src/Controller/SearchElasticIntervalsController.php
+++ Elasticsearch/src/Controller/SearchElasticIntervalsController.php
@@ -17,7 +17,7 @@ class SearchElasticIntervalsController extends AbstractController
 
     public function __construct(CreateClientElasticSearch $clientElasticSearch)
     {
-        $this->clientElasticSearch = $clientElasticSearch;
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
     }
 
     /**
@@ -25,6 +25,28 @@ class SearchElasticIntervalsController extends AbstractController
      */
     public function index(): Response
     {
+        $params = [
+            'index' => 'my_index',
+            'size' => 99,
+            'body'  => [
+                'query' => [
+                    'match' => [
+                        'testField.creditCardType' =>  [
+                            'query' => 'MasterCard',
+                            "fuzziness" => "AUTO"
+                        ]
+                    ]
+                ],
+                'sort' => [
+                    'testField.id' => [
+                        'order' => 'desc'
+                    ]
+                ]
+            ]
+        ];
+        $response = $this->clientElasticSearch->search($params);
+        dd($response);
+
         return $this->render('search_elastic_intervals/index.html.twig', [
             'controller_name' => 'SearchElasticIntervalsController',
         ]);
