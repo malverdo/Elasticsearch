d7bb7e6f4b6ae0262d84f7e2264219bce3d31bde
malverdo <malverdo11@mail.ru>
2022-01-24 08:57:56 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/SearchElasticIntervalsController.php
+++ Elasticsearch/src/Controller/SearchElasticIntervalsController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchElasticIntervalsController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch;
+    }
+
     /**
      * @Route("/search/elastic/intervals", name="search_elastic_intervals")
      */
