56df70b69392a1ffc5487c9c0d4a81d5424b0ad4
malverdo <malverdo11@mail.ru>
2022-01-25 08:43:27 +0300

    create aggs

--- Elasticsearch/src/Controller/SearchElasticAggsController.php
+++ Elasticsearch/src/Controller/SearchElasticAggsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,55 @@ use Symfony\Component\Routing\Annotation\Route;
 class SearchElasticAggsController extends AbstractController
 {
     /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
+    /**
      * @Route("/search/elastic/aggs", name="search_elastic_aggs")
      */
     public function index(): Response
     {
+
+        $params = [
+            'index' => 'my_index',
+            'size' => 99,
+            'body'  => [
+                'query' => [
+                    'bool' => [
+                        'must' => [
+                            [
+                                'match' => [
+                                    'testField.creditCardType' =>  'MasterCard'
+                                ]
+                            ]
+                        ]
+                    ]
+                 ],
+                "aggs" => [
+                    "my-agg-name" => [
+                        "terms" => [
+                            "field" => "ban"
+                        ]
+                    ]
+                ],
+
+                'sort' => [
+                    'testField.id' => [
+                        'order' => 'desc'
+
+                    ]
+                ]
+            ]
+        ];
+        $response = $this->clientElasticSearch->search($params);
+        dd($response);
+
         return $this->render('search_elastic_aggs/index.html.twig', [
             'controller_name' => 'SearchElasticAggsController',
         ]);
