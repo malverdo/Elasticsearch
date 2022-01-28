1d89462b17b8b3ccc503a8af14095579f7ce0d89
malverdo <malverdo11@mail.ru>
2022-01-28 08:49:44 +0300

    add client

--- Elasticsearch/src/Controller/SearchYourData/FilterPostSearchResultsController.php
+++ Elasticsearch/src/Controller/SearchYourData/FilterPostSearchResultsController.php
@@ -2,12 +2,25 @@
 
 namespace App\Controller\SearchYourData;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FilterPostSearchResultsController extends AbstractController
 {
+
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/filter/post/search/results", name="filter_post_search_results")
      */
