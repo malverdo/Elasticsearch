67e207e76482a3087039df3e5abcee634e0fc436
malverdo <malverdo11@mail.ru>
2022-01-28 08:49:20 +0300

    create FilterPostSearchResultsController

---
+++ Elasticsearch/src/Controller/SearchYourData/FilterPostSearchResultsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\SearchYourData;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FilterPostSearchResultsController extends AbstractController
+{
+    /**
+     * @Route("/filter/post/search/results", name="filter_post_search_results")
+     */
+    public function index(): Response
+    {
+
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 1,
+            'body'  => [
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            ['term' => ['_doc.company' => 'Lindgren Inc']],
+                            ['term' => ['_doc.ban' => true]],
+                        ]
+                    ]
+                ],
+                'aggs' => [
+                    'models' => [
+                        'terms' => ['field' => '_doc.creditCardType']
+                    ]
+                ]
+            ]
+        ];
+        $response = $this->clientElasticSearch->search($params);
+        dd($response);
+
+        return $this->render('filter_post_search_results/index.html.twig', [
+            'controller_name' => 'FilterPostSearchResultsController',
+        ]);
+    }
+}
