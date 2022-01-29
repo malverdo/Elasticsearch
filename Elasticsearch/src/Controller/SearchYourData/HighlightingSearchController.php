1f69f3107cdc3addd9831bcca1c8248ae8f23065
malverdo <malverdo11@mail.ru>
2022-01-29 12:06:53 +0300

    add query highlight

--- Elasticsearch/src/Controller/SearchYourData/HighlightingSearchController.php
+++ Elasticsearch/src/Controller/SearchYourData/HighlightingSearchController.php
@@ -25,6 +25,32 @@ class HighlightingSearchController extends AbstractController
      */
     public function index(): Response
     {
+
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 1,
+            'body'  => [
+                'query' => [
+                    'match' => [
+                        "_doc.data.aboutMe" => 'tempora'
+                    ]
+                ],
+                'highlight' => [
+                    'fields' => [
+                        '_doc.data.aboutMe' => [
+                            "type" => 'unified',
+                            "pre_tags" => ["<b>"],
+                             "post_tags" => ["</b>"],
+                            'boundary_scanner' => 'word',
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $response = $this->clientElasticSearch->search($params);
+        dd($response);
+
         return $this->render('highlighting_search/index.html.twig', [
             'controller_name' => 'HighlightingSearchController',
         ]);
