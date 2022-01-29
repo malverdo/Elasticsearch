a9784be5f4b5be9632b71d440d4f6470ca64fde6
malverdo <malverdo11@mail.ru>
2022-01-29 12:20:31 +0300

    add clientElasticSearch

--- Elasticsearch/src/Controller/SearchYourData/PaginateSearchController.php
+++ Elasticsearch/src/Controller/SearchYourData/PaginateSearchController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\SearchYourData;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PaginateSearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/paginate/search", name="paginate_search")
      */
