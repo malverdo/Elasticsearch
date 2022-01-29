b086ba29aa737f2201c762db11b3b66a7f3d60f1
malverdo <malverdo11@mail.ru>
2022-01-29 10:55:40 +0300

    add clientElasticSearch

--- Elasticsearch/src/Controller/SearchYourData/HighlightingSearchController.php
+++ Elasticsearch/src/Controller/SearchYourData/HighlightingSearchController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\SearchYourData;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HighlightingSearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/highlighting/search", name="highlighting_search")
      */
