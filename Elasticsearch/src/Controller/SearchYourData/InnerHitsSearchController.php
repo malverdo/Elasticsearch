06d81aa7f0941e5cb8595bb3cb26f43bc7d0552e
malverdo <malverdo11@mail.ru>
2022-01-30 09:16:08 +0300

    add $params

--- Elasticsearch/src/Controller/SearchYourData/InnerHitsSearchController.php
+++ Elasticsearch/src/Controller/SearchYourData/InnerHitsSearchController.php
@@ -26,7 +26,20 @@ class InnerHitsSearchController extends AbstractController
     public function index(): Response
     {
 
-
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 1,
+            'body'  => [
+                'query' => [
+                    'match' => [
+                        "_doc.data.aboutMe" => 'tempora'
+                    ]
+                ]
+            ]
+        ];
+        $response = $this->clientElasticSearch->search($params);
+        dd($response);
 
         return $this->render('inner_hits_search/index.html.twig', [
             'controller_name' => 'InnerHitsSearchController',
