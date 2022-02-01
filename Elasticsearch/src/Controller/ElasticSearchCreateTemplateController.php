e24815d2b782af34d587fbbf94a3e3b8bbb84eca
malverdo <malverdo11@mail.ru>
2022-02-01 07:19:52 +0300

    add ElasticSearchCreateTemplateController

---
+++ Elasticsearch/src/Controller/ElasticSearchCreateTemplateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ElasticSearchCreateTemplateController extends AbstractController
+{
+    /**
+     * @Route("/elastic/search/create/template", name="elastic_search_create_template")
+     */
+    public function index(): Response
+    {
+        return $this->render('elastic_search_create_template/index.html.twig', [
+            'controller_name' => 'ElasticSearchCreateTemplateController',
+        ]);
+    }
+}
