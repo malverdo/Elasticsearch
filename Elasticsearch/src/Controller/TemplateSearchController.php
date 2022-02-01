3b1da02893d7191b8caf56dcdc741f647da59b49
malverdo <malverdo11@mail.ru>
2022-02-01 08:09:59 +0300

    add TemplateSearchController

---
+++ Elasticsearch/src/Controller/TemplateSearchController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TemplateSearchController extends AbstractController
+{
+    /**
+     * @Route("/template/search", name="template_search")
+     */
+    public function index(): Response
+    {
+        return $this->render('template_search/index.html.twig', [
+            'controller_name' => 'TemplateSearchController',
+        ]);
+    }
+}
