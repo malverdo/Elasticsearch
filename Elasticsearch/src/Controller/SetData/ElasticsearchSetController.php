c10682f648365dc169d1d1c3f2858e39d0417f2b
malverdo <malverdo11@mail.ru>
2022-02-02 08:53:54 +0300

    ref 10

--- Elasticsearch/src/Controller/SetData/ElasticsearchSetController.php
+++ Elasticsearch/src/Controller/SetData/ElasticsearchSetController.php
@@ -39,7 +39,7 @@ class ElasticsearchSetController extends AbstractController
     {
         $client = $this->clientElasticSearch->getClient();
 
-        for ($i = 1; $i <= 12234; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
 
 
         $params = [
