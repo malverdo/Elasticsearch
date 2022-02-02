e6584b269acdaeeba3f47a1123dd7af233482680
malverdo <malverdo11@mail.ru>
2022-02-02 08:03:49 +0300

    add SortSearchResultsController

---
+++ Elasticsearch/src/Controller/SortSearchResultsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortSearchResultsController extends AbstractController
+{
+    /**
+     * @Route("/sort/search/results", name="sort_search_results")
+     */
+    public function index(): Response
+    {
+        return $this->render('sort_search_results/index.html.twig', [
+            'controller_name' => 'SortSearchResultsController',
+        ]);
+    }
+}
