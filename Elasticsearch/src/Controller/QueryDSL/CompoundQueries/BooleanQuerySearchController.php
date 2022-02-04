c8e9c42f0c07b046d24e58c692d5acedef5d33b8
malverdo <malverdo11@mail.ru>
2022-02-04 07:57:42 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/CompoundQueries/BooleanQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/CompoundQueries/BooleanQuerySearchController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller\QueryDSL\CompoundQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BooleanQuerySearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/boolean/query/search", name="boolean_query_search")
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 21,
+            'body'  => [
+                'query' => [
+
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('boolean_query_search/index.html.twig', [
             'controller_name' => 'BooleanQuerySearchController',
         ]);
