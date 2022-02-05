102c2214cd9cc27304850bacd0ddf4f76c996e66
malverdo <malverdo11@mail.ru>
2022-02-05 12:26:53 +0300

    add card_index

--- Elasticsearch/src/Controller/QueryDSL/CompoundQueries/BoostingQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/CompoundQueries/BoostingQuerySearchController.php
@@ -30,6 +30,23 @@ class BoostingQuerySearchController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 21,
+            'body' => [
+                'query' => [
+                    '2' => 2
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
+
         return $this->render('boosting_query_search/index.html.twig', [
             'controller_name' => 'BoostingQuerySearchController',
         ]);
