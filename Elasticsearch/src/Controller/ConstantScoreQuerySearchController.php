07f92226a84756a90d6c8b1c8214d57188746b42
malverdo <malverdo11@mail.ru>
2022-02-05 16:16:45 +0300

    add ConstantScoreQuerySearchController

---
+++ Elasticsearch/src/Controller/ConstantScoreQuerySearchController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConstantScoreQuerySearchController extends AbstractController
+{
+    /**
+     * @Route("/constant/score/query/search", name="constant_score_query_search")
+     */
+    public function index(): Response
+    {
+        return $this->render('constant_score_query_search/index.html.twig', [
+            'controller_name' => 'ConstantScoreQuerySearchController',
+        ]);
+    }
+}
