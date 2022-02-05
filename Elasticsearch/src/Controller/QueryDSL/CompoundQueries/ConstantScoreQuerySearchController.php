b58430e6ff9d84931282bc81f230087e0f290136
malverdo <malverdo11@mail.ru>
2022-02-05 16:29:05 +0300

    add card_index

--- Elasticsearch/src/Controller/QueryDSL/CompoundQueries/ConstantScoreQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/CompoundQueries/ConstantScoreQuerySearchController.php
@@ -27,6 +27,33 @@ class ConstantScoreQuerySearchController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 21,
+            'body' => [
+                'query' => [
+                    'boosting' => [
+                        'positive' => [
+                            'term' => [
+                                '_doc.data.aboutMe' => 'ipsam'
+                            ]
+                        ],
+                        'negative' => [
+                            'term' => [
+                                '_doc.data.aboutMe' => 'perferendis'
+                            ]
+                        ],
+                        "negative_boost" => 0.5
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
         return $this->render('constant_score_query_search/index.html.twig', [
             'controller_name' => 'ConstantScoreQuerySearchController',
         ]);
