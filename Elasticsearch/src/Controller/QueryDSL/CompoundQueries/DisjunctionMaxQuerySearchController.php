2df90114d7bf4bf13cebf4e360a0a5fee874d27a
malverdo <malverdo11@mail.ru>
2022-02-05 17:33:25 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/CompoundQueries/DisjunctionMaxQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/CompoundQueries/DisjunctionMaxQuerySearchController.php
@@ -1,13 +1,25 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\QueryDSL\CompoundQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DisjunctionMaxQuerySearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/disjunction/max/query/search", name="disjunction_max_query_search")
      */
