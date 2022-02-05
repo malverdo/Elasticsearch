eef561793e1b32ef5aa7ddd2a6be1fbaaf936940
malverdo <malverdo11@mail.ru>
2022-02-05 17:41:39 +0300

    add data

--- Elasticsearch/src/Controller/QueryDSL/CompoundQueries/DisjunctionMaxQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/CompoundQueries/DisjunctionMaxQuerySearchController.php
@@ -25,6 +25,37 @@ class DisjunctionMaxQuerySearchController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'query' => [
+                    'dis_max' => [
+                        'queries' => [
+                            [
+                                'term' => [
+                                    '_doc.data.aboutMe' => 'ipsam'
+                                ]
+                            ],
+                            [
+                                'term' => [
+                                    '_doc.data.aboutMe' => 'perferendis'
+                                ]
+                            ]
+                        ],
+                        "tie_breaker" => 0.9
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
+
         return $this->render('disjunction_max_query_search/index.html.twig', [
             'controller_name' => 'DisjunctionMaxQuerySearchController',
         ]);
