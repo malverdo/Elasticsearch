feb5ec587cd28e99026b10651e4ab0b77d7e0949
malverdo <malverdo11@mail.ru>
2022-02-05 12:25:53 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/CompoundQueries/BoostingQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/CompoundQueries/BoostingQuerySearchController.php
@@ -2,12 +2,29 @@
 
 namespace App\Controller\QueryDSL\CompoundQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BoostingQuerySearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+
+    /**
+     * @param CreateClientElasticSearch $clientElasticSearch
+     */
+    public function __construct(
+        CreateClientElasticSearch $clientElasticSearch
+    ) {
+        $this->clientElasticSearch = $clientElasticSearch;
+    }
+
     /**
      * @Route("/boosting/query/search", name="boosting_query_search")
      */
