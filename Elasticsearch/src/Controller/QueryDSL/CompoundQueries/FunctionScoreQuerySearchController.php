7266f8055755cb509be587c69ec4b4293f8d6e01
malverdo <malverdo11@mail.ru>
2022-02-06 07:52:43 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/CompoundQueries/FunctionScoreQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/CompoundQueries/FunctionScoreQuerySearchController.php
@@ -2,12 +2,25 @@
 
 namespace App\Controller\QueryDSL\CompoundQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FunctionScoreQuerySearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
+
     /**
      * @Route("/function/score/query/search", name="function_score_query_search")
      */
