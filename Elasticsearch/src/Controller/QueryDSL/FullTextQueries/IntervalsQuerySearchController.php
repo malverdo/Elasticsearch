4d7ee2382c17329e8b7588fc71607754f93dec74
malverdo <malverdo11@mail.ru>
2022-02-06 08:31:26 +0300

    ref IntervalsQuerySearchController

--- Elasticsearch/src/Controller/QueryDSL/FullTextQueries/IntervalsQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/FullTextQueries/IntervalsQuerySearchController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\QueryDSL\FullTextQueries;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
