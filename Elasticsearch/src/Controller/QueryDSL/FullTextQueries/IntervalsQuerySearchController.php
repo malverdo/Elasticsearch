e6caf50002c68e4b64cf2a3be32e1fdbd9f18fdc
malverdo <malverdo11@mail.ru>
2022-02-06 08:32:04 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/FullTextQueries/IntervalsQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/FullTextQueries/IntervalsQuerySearchController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller\QueryDSL\FullTextQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IntervalsQuerySearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/intervals/query/search", name="intervals_query_search")
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'query' => [
+
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('intervals_query_search/index.html.twig', [
             'controller_name' => 'IntervalsQuerySearchController',
         ]);
