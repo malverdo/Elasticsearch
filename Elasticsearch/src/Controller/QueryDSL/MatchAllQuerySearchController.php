4f9c60ffa33f5fce0ef37ce6247d06b29f915266
malverdo <malverdo11@mail.ru>
2022-02-07 08:47:59 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/MatchAllQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/MatchAllQuerySearchController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\QueryDSL;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MatchAllQuerySearchController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/match/all/query/search", name="match_all_query_search")
      */
