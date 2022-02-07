f5bd49a71c2f19a45aa474936c31ebf0e008f9b8
malverdo <malverdo11@mail.ru>
2022-02-07 08:48:30 +0300

    add index

--- Elasticsearch/src/Controller/QueryDSL/MatchAllQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/MatchAllQuerySearchController.php
@@ -25,6 +25,20 @@ class MatchAllQuerySearchController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('match_all_query_search/index.html.twig', [
             'controller_name' => 'MatchAllQuerySearchController',
         ]);
