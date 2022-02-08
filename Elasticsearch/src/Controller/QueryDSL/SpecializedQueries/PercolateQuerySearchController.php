437b696773e38a3ed0674cc56d910d3d170b5921
malverdo <malverdo11@mail.ru>
2022-02-08 09:37:46 +0300

    add param

--- Elasticsearch/src/Controller/QueryDSL/SpecializedQueries/PercolateQuerySearchController.php
+++ Elasticsearch/src/Controller/QueryDSL/SpecializedQueries/PercolateQuerySearchController.php
@@ -25,6 +25,27 @@ class PercolateQuerySearchController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'query' => [
+                    'more_like_this' => [
+                        "fields" => ["_doc.data.aboutMe"],
+                        "like" => 'consequatur null',
+                        "min_term_freq" => 1,
+                        "max_query_terms" => 12
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('percolate_query_search/index.html.twig', [
             'controller_name' => 'PercolateQuerySearchController',
         ]);
