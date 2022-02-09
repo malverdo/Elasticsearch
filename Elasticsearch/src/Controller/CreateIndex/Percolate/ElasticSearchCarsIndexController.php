67d0394fa3a4cfdf0081eeee4e3a1c7b1dc62501
malverdo <malverdo11@mail.ru>
2022-02-09 06:51:39 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/CreateIndex/Percolate/ElasticSearchCarsIndexController.php
+++ Elasticsearch/src/Controller/CreateIndex/Percolate/ElasticSearchCarsIndexController.php
@@ -1,13 +1,30 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\CreateIndex\Percolate;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ElasticSearchCarsIndexController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+
+    /**
+     * @param CreateClientElasticSearch $clientElasticSearch
+     */
+    public function __construct(
+        CreateClientElasticSearch $clientElasticSearch
+    ) {
+        $this->clientElasticSearch = $clientElasticSearch;
+    }
+
     /**
      * @Route("/elastic/search/cars/index", name="elastic_search_cars_index")
      */
