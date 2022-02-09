833ce1b43562f29e3f9ac386fda2dfa43b2010c0
malverdo <malverdo11@mail.ru>
2022-02-09 06:55:00 +0300

    add ElasticSearchPercolatorCarsController

---
+++ Elasticsearch/src/Controller/ElasticSearchPercolatorCarsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ElasticSearchPercolatorCarsController extends AbstractController
+{
+    /**
+     * @Route("/elastic/search/percolator/cars", name="elastic_search_percolator_cars")
+     */
+    public function index(): Response
+    {
+        return $this->render('elastic_search_percolator_cars/index.html.twig', [
+            'controller_name' => 'ElasticSearchPercolatorCarsController',
+        ]);
+    }
+}
