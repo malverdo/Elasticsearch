7da6902643ef49dd7a011cb2f24b7e33612e778e
malverdo <malverdo11@mail.ru>
2022-02-10 08:25:56 +0300

    add ScriptQueryController

---
+++ Elasticsearch/src/Controller/ScriptQueryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ScriptQueryController extends AbstractController
+{
+    /**
+     * @Route("/script/query", name="script_query")
+     */
+    public function index(): Response
+    {
+        return $this->render('script_query/index.html.twig', [
+            'controller_name' => 'ScriptQueryController',
+        ]);
+    }
+}
