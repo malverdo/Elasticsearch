2411210dd958d141e4b25f4d73719d44602d4012
malverdo <malverdo11@mail.ru>
2022-02-11 08:29:03 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/SpecializedQueries/PinnedQueryController.php
+++ Elasticsearch/src/Controller/QueryDSL/SpecializedQueries/PinnedQueryController.php
@@ -2,12 +2,25 @@
 
 namespace App\Controller\QueryDSL\SpecializedQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinnedQueryController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
+
     /**
      * @Route("/pinned/query", name="pinned_query")
      */
