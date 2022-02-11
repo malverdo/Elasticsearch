d1ebcfdc2dace540ad830e8c68eb8431f00144fc
malverdo <malverdo11@mail.ru>
2022-02-11 08:45:15 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/ExistsQueryController.php
+++ Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/ExistsQueryController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\QueryDSL\TermLevelQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExistsQueryController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/exists/query", name="exists_query")
      */
