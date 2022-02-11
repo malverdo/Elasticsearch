a69ab70f95fc0b1a79425523b3e77539e7690753
malverdo <malverdo11@mail.ru>
2022-02-11 09:09:20 +0300

    add fuzzy

--- Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/FuzzyQueryController.php
+++ Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/FuzzyQueryController.php
@@ -25,6 +25,28 @@ class FuzzyQueryController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'must_not' => [
+                            'exists' => [
+                                'field' => "_doc.data.aboutMe"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('Аfuzzy_query/index.html.twig', [
             'controller_name' => 'АFuzzyQueryController',
         ]);
