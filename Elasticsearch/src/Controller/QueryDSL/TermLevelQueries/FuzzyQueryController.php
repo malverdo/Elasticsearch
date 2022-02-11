8c20067d7651f2297a90d15bd537b78924b4eeb1
malverdo <malverdo11@mail.ru>
2022-02-11 09:09:00 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/FuzzyQueryController.php
+++ Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/FuzzyQueryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\QueryDSL\TermLevelQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class FuzzyQueryController extends AbstractController
 {
 
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
 
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
 
     /**
      * @Route("/fuzzy/query", name="_fuzzy_query")
