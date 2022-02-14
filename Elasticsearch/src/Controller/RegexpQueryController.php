7e4d398400107c3f609af7a1421aeb6279be0d73
malverdo <malverdo11@mail.ru>
2022-02-14 08:02:21 +0300

    add RegexpQueryController

---
+++ Elasticsearch/src/Controller/RegexpQueryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegexpQueryController extends AbstractController
+{
+    /**
+     * @Route("/regexp/query", name="regexp_query")
+     */
+    public function index(): Response
+    {
+        return $this->render('regexp_query/index.html.twig', [
+            'controller_name' => 'RegexpQueryController',
+        ]);
+    }
+}
