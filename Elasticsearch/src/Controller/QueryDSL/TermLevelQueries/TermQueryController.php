929f5a7ba59da8e09eb237c376e7d97af59a7a66
malverdo <malverdo11@mail.ru>
2022-02-14 08:20:40 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/TermQueryController.php
+++ Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/TermQueryController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\QueryDSL\TermLevelQueries;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TermQueryController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/term/query", name="term_query")
      */
