ad8e545a8d1a933cbc63fed24f548fd332e6b860
malverdo <malverdo11@mail.ru>
2022-02-14 08:03:51 +0300

    add $params

--- Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/RegexpQueryController.php
+++ Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/RegexpQueryController.php
@@ -25,6 +25,27 @@ class RegexpQueryController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'query' => [
+                    'range' => [
+                        '_doc.dateTimeRegistrationCard.date' => [
+                            "gte" => "now-10M/M",
+                            "lte" => "now/d"
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('regexp_query/index.html.twig', [
             'controller_name' => 'RegexpQueryController',
         ]);
