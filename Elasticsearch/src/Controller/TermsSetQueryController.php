14937a4d5782e8d580da502cae9d89f78c4a7244
malverdo <malverdo11@mail.ru>
2022-02-15 08:35:40 +0300

    add TermsSetQueryController

---
+++ Elasticsearch/src/Controller/TermsSetQueryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TermsSetQueryController extends AbstractController
+{
+    /**
+     * @Route("/terms/set/query", name="terms_set_query")
+     */
+    public function index(): Response
+    {
+        return $this->render('terms_set_query/index.html.twig', [
+            'controller_name' => 'TermsSetQueryController',
+        ]);
+    }
+}
