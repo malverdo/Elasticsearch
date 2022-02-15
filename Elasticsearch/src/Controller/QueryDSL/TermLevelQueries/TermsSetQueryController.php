1db0708292135a86a1552009b12d895068af3436
malverdo <malverdo11@mail.ru>
2022-02-15 08:36:43 +0300

    add $params

--- Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/TermsSetQueryController.php
+++ Elasticsearch/src/Controller/QueryDSL/TermLevelQueries/TermsSetQueryController.php
@@ -25,6 +25,27 @@ class TermsSetQueryController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'query' => [
+                    'term' => [
+                        '_doc.creditCardType' => [
+                            "value" => 'Visa',
+                            "boost" => 1.0
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('terms_set_query/index.html.twig', [
             'controller_name' => 'TermsSetQueryController',
         ]);
