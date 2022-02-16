3f350628d794e065425655225b027d37766c1582
malverdo <malverdo11@mail.ru>
2022-02-16 08:32:29 +0300

    add $params

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/AdjacencyMatrixAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/AdjacencyMatrixAggregationController.php
@@ -25,6 +25,24 @@ class AdjacencyMatrixAggregationController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'query' => [
+                    'regexp' => [
+
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('adjacency_matrix_aggregation/index.html.twig', [
             'controller_name' => 'AdjacencyMatrixAggregationController',
         ]);
