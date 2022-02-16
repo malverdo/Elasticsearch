85332bd60b9cc041c98d638af6fd7ee39da65d6c
malverdo <malverdo11@mail.ru>
2022-02-16 08:32:10 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/AdjacencyMatrixAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/AdjacencyMatrixAggregationController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\Aggregations\BucketAggregations;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdjacencyMatrixAggregationController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/adjacency/matrix/aggregation", name="adjacency_matrix_aggregation")
      */
