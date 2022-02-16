da1f58114b1c9ce4402bfab1ac969ddd15bdae68
malverdo <malverdo11@mail.ru>
2022-02-16 08:31:39 +0300

    ref AdjacencyMatrixAggregationController

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/AdjacencyMatrixAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/AdjacencyMatrixAggregationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Aggregations\BucketAggregations;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
