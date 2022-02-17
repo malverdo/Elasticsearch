10a8abccf558290158fca6c9c66134ab4867e96e
malverdo <malverdo11@mail.ru>
2022-02-17 08:57:27 +0300

    add HistogramAggregationController

---
+++ Elasticsearch/src/Controller/HistogramAggregationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HistogramAggregationController extends AbstractController
+{
+    /**
+     * @Route("/histogram/aggregation", name="histogram_aggregation")
+     */
+    public function index(): Response
+    {
+        return $this->render('histogram_aggregation/index.html.twig', [
+            'controller_name' => 'HistogramAggregationController',
+        ]);
+    }
+}
