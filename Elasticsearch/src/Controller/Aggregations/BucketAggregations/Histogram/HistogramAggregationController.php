8f88bfa84e316db29025c630102e3842a1f8763d
malverdo <malverdo11@mail.ru>
2022-02-17 08:58:47 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/Histogram/HistogramAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/Histogram/HistogramAggregationController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\Aggregations\BucketAggregations\Histogram;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HistogramAggregationController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/histogram/aggregation", name="histogram_aggregation")
      */
