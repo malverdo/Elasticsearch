c82440371bb339b0e1c85682db512261313568fe
malverdo <malverdo11@mail.ru>
2022-02-18 09:34:14 +0300

    add $params

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/CompositeAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/CompositeAggregationController.php
@@ -25,6 +25,26 @@ class CompositeAggregationController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 51,
+            'body' => [
+                'aggs' => [
+                    'interactions' => [
+                        'adjacency_matrix' => [
+
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
         return $this->render('composite_aggregation/index.html.twig', [
             'controller_name' => 'CompositeAggregationController',
         ]);
