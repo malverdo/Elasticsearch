9fee148368e4ef161f5eaf63e75ad106f2c67e6c
malverdo <malverdo11@mail.ru>
2022-02-18 09:33:50 +0300

    add CreateClientElasticSearch

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/CompositeAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/CompositeAggregationController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller\Aggregations\BucketAggregations;
 
+use App\Service\CreateClientElasticSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompositeAggregationController extends AbstractController
 {
+
+    /**
+     * @var CreateClientElasticSearch
+     */
+    private $clientElasticSearch;
+
+    public function __construct(CreateClientElasticSearch $clientElasticSearch)
+    {
+        $this->clientElasticSearch = $clientElasticSearch->getClient();
+    }
+
     /**
      * @Route("/composite/aggregation", name="composite_aggregation")
      */
