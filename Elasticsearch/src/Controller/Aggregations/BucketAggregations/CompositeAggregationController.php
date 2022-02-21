8934ccd97766bfa5a530fd1d3bebae5bc1ce2952
malverdo <malverdo11@mail.ru>
2022-02-21 08:48:14 +0300

    TODO

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/CompositeAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/CompositeAggregationController.php
@@ -35,7 +35,7 @@ class CompositeAggregationController extends AbstractController
                 'aggs' => [
                     'interactions' => [
                         'adjacency_matrix' => [
-
+                            // TODO Composite aggregation
                         ]
                     ]
                 ]
