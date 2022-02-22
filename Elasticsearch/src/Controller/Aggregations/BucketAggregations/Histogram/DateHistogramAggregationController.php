4772f3aa4c5977271cd286e056073f52ec689036
malverdo <malverdo11@mail.ru>
2022-02-22 06:38:45 +0300

    add params date histogram

--- Elasticsearch/src/Controller/Aggregations/BucketAggregations/Histogram/DateHistogramAggregationController.php
+++ Elasticsearch/src/Controller/Aggregations/BucketAggregations/Histogram/DateHistogramAggregationController.php
@@ -25,6 +25,27 @@ class DateHistogramAggregationController extends AbstractController
      */
     public function index(): Response
     {
+
+        $client = $this->clientElasticSearch;
+        $params = [
+            'index' => 'card_index',
+            'track_total_hits' => true,
+            'size' => 5,
+            'body' => [
+                'aggs' => [
+                    'latency_buckets' => [
+                        'histogram' => [
+
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        dd($response);
+
+
         return $this->render('date_histogram_aggregation/index.html.twig', [
             'controller_name' => 'DateHistogramAggregationController',
         ]);
